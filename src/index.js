b97c0750b4aa7517efecddb0b9ef0d0606dab9f6
Ajaypal Cheema <ajaypalster@gmail.com>
2021-12-15 21:23:01 -0500

    refactor to start things off

--- src/index.js
+++ src/index.js
@@ -3,34 +3,25 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { MoralisProvider } from "react-moralis";
 import "./index.css";
-import QuickStart from "components/QuickStart";
 import { MoralisDappProvider } from "./providers/MoralisDappProvider/MoralisDappProvider";
 
 const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
 const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
 
 const Application = () => {
-  if (APP_ID && SERVER_URL)
-    return (
-      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-        <MoralisDappProvider>
-          <App />
-        </MoralisDappProvider>
-      </MoralisProvider>
-    );
-  else {
-    return (
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <QuickStart />
-      </div>
-    );
-  }
+	return (
+		<MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+			<MoralisDappProvider>
+				<App />
+			</MoralisDappProvider>
+		</MoralisProvider>
+	);
 };
 
 /** Get your free Moralis Account https://moralis.io/ */
 ReactDOM.render(
-  <React.StrictMode>
-    <Application />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Application />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
