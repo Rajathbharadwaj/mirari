09ddbac9d2a63d64bc7c0c29564a747999c1fa8d
Ajaypal Cheema <ajaypalster@gmail.com>
2021-12-17 14:01:26 -0500

    add wte logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,29 @@ import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-do
 import Account from "components/Account";
 import Wallet from "components/Wallet";
 import { Grid, Stack, Typography } from "@mui/material";
+import Logo from "./Logo.png";
 
 const styles = {
 	root: { height: "100vh", background: "#000000", color: "#fff", fontFamily: "Roboto, sans-serif" },
+	logoLink: {
+		textDecoration: "none",
+		color: "unset",
+	},
+	logo: {
+		height: "80px",
+		width: "120px",
+	},
 	header: {
 		position: "fixed",
 		paddingTop: "8px",
 		background: "#121212",
 	},
-	headerItem: { paddingTop: "8px" },
+	headerItem: { paddingTop: "22px" },
 	content: {
 		display: "flex",
 		justifyContent: "center",
 		color: "#6e6e6e",
-		paddingTop: "6%",
+		paddingTop: "10%",
 	},
 	link: {
 		textDecoration: "none",
@@ -46,7 +55,9 @@ const App = () => {
 			<Grid container style={styles.root}>
 				<Grid container item alignItems="flex-start" style={styles.header}>
 					<Grid item xs={2}>
-						<Logo />
+						<NavLink to="/" style={styles.logoLink}>
+							<img alt="Watch To Earn Logo" style={styles.logo} src={Logo} />
+						</NavLink>
 					</Grid>
 					<Grid item xs={4} style={styles.headerItem}>
 						<Stack direction="row" justifyContent="center" alignItems="flex-start" spacing={6}>
@@ -67,7 +78,7 @@ const App = () => {
 					<Grid item xs={4} style={styles.headerItem}>
 						Search bar goes here
 					</Grid>
-					<Grid item xs={2}>
+					<Grid item xs={2} style={{ paddingTop: "6px" }}>
 						<Account />
 					</Grid>
 				</Grid>
@@ -85,18 +96,5 @@ const App = () => {
 		</Router>
 	);
 };
-
-export const Logo = () => (
-	<svg width="80" height="50" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<path
-			d="M0 10C0 4.47715 4.47715 0 10 0H20C25.5228 0 30 4.47715 30 10V20C30 25.5228 25.5228 30 20 30H10C4.47715 30 0 25.5228 0 20V10Z"
-			fill="#E84142"
-		/>
-		<path
-			d="M20.2914 15.3898C20.8111 14.4921 21.6497 14.4921 22.1693 15.3898L25.4056 21.0709C25.9252 21.9685 25.5 22.7008 24.4607 22.7008H17.941C16.9134 22.7008 16.4882 21.9685 16.9961 21.0709L20.2914 15.3898ZM14.0315 4.45277C14.5512 3.55513 15.378 3.55513 15.8977 4.45277L16.6182 5.75198L18.3189 8.74017C18.7323 9.59056 18.7323 10.5945 18.3189 11.4449L12.6142 21.3307C12.0945 22.1339 11.2323 22.6417 10.2756 22.7008H5.53942C4.50005 22.7008 4.07485 21.9803 4.59454 21.0709L14.0315 4.45277Z"
-			fill="white"
-		/>
-	</svg>
-);
 
 export default App;
