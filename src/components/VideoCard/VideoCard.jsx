69bfed21b76fee5a51f8969532a651fcd814f775
Ajaypal Cheema <ajaypalster@gmail.com>
2021-12-21 18:27:15 -0500

    boxshadow

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -4,7 +4,15 @@ import { Typography, Card, CardMedia, CardContent } from "@mui/material";
 const VideoCard = (props) => {
 	const { imgSrc, content, altTag } = props;
 	return (
-		<Card sx={{ maxWidth: 345 }}>
+		<Card
+			sx={{
+				maxWidth: 345,
+				":hover": {
+					cursor: "pointer",
+					boxShadow: "0 10px 10px #c4c4c4",
+				},
+			}}
+		>
 			<CardMedia component="img" image={imgSrc} alt={altTag} />
 			<CardContent>
 				<Typography>{content}</Typography>
